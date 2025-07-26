7b78f8581fe87053d8c1fb634be170fa1ad73345
Ruddyard Castro <rcastroc2@miumg.edu.gt>
2025-07-26 13:17:43 -0600

    Se crearon las subventans pero no son visibles

--- Automatas y Leng 2507.py
+++ Automatas y Leng 2507.py
@@ -7,9 +7,13 @@ class Aplicacion:
         self.root = root
         self.root.title("Interfaz ")
         self.root.geometry("800x600")
-        self.crear_menu()
-    
-    def crear_menu(self):
+        self.crear_menu1()
+        self.crear_marco_principal()
+
+
+
+
+    def crear_menu1(self):
         menubar = tk.Menu(self.root)
         
         #-- Menú Archivo logica basada en word 
@@ -22,6 +26,12 @@ class Aplicacion:
         menubar.add_cascade(label="Archivo", menu=menu_archivo)
         #? Las funciones de guardar guardar como y cerrar se definen abajo para poder tener sepradao los nombre la parte grafica de las funciones 
         self.root.config(menu=menubar)
+
+
+
+
+
+
     #? Aqui estan las funciones de los nombre que se miran en pantalla 
     def guardar(self):
         messagebox.showinfo("Guardar", "Función Guardar ")
@@ -35,6 +45,34 @@ class Aplicacion:
         if messagebox.askokcancel("Cerrar", "¿Está seguro que desea cerrar la aplicación?"):
             #? Creo que breack no funciona a qui por ser visual no no un funcion 
             self.root.destroy()
+
+
+    def crear_marco_principal(self):
+        """Crea el contenedor principal para las ventanas"""
+        self.marco_principal = tk.Frame(self.root, bg="#f0f0f0", padx=10, pady=10)
+        self.marco_principal.pack(fill=tk.BOTH, expand=True)
+        
+        # Etiqueta temporal para ver el marco
+        tk.Label(self.marco_principal, text="Proyecto de automatas", 
+                bg="lightblue").pack(fill=tk.X, pady=50)
+        self.ventana1 = tk.LabelFrame(
+            self.marco_principal, 
+            text="Hola",  
+            padx=15, 
+            pady=15,
+            
+        )
+
+        self.ventana_secundaria = tk.LabelFrame(
+            self.marco_principal,
+            text="Configuración",
+            padx=15,
+            pady=15
+        )
+        
+       
+      
+
 
 # Ejecutar para probar
 if __name__ == "__main__":
